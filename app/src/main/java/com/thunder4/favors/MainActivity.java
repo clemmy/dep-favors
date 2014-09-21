91dd56d94a7d443c2150eb7c7c32fbe721f120cb
unknown <clement.hoang24@gmail.com>
2014-09-21 00:06:34 -0400

    Removed naming conflicts with loginactivity

--- app/src/main/java/com/thunder4/favors/MainActivity.java
+++ app/src/main/java/com/thunder4/favors/MainActivity.java
@@ -1,17 +1,14 @@
 package com.thunder4.favors;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-public class LoginActivity extends Fragment {
+public class MainActivity extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +17,7 @@ public class LoginActivity extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_login,
+        View view = inflater.inflate(R.layout.activity_main,
                 container, false);
         return view;
     }

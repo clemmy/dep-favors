bbd6a5e66d903cc15d5de7f29d96b5322f98e9a8
unknown <clement.hoang24@gmail.com>
2014-09-21 07:48:00 -0400

    Miscellaneous skeleton classes added

---
+++ app/src/main/java/com/thunder4/favors/StringUriBuilder.java
@@ -0,0 +1,13 @@
+package com.thunder4.favors;
+
+/**
+ * Created by Clement on 9/21/2014.
+ */
+public class StringUriBuilder {
+    String url = "";
+    String param1 = "";
+
+    public String Join(String uri, String param1 ) {
+        return "";
+    }
+}
